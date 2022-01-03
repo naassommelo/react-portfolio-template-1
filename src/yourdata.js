8d8df8b1b9105ed9d996e8f28ce0c022cba8a482
naassommelo <naassom.dsgn@gmail.com>
2022-01-03 00:39:55 -0300

    atualização about

--- src/yourdata.js
+++ src/yourdata.js
@@ -118,9 +118,6 @@ export default {
     "I started to study a lot of design content and in 2018, when I was 15 years old, I received a scolarship to study at the Colégio IAPE and be part of they internal marketing team as graphic designer and videomaker.",
   aboutParaThree:
     "I always liked to play videogames, and the Counter-Strike was my first contact with the world of the esports and started to make some concepts based on esports teams, so, at the end of 2019 I received a message to be part of the OnFire Produtora, an college esports producer. After this I joined the Botafogo Esports as art director and community manager.",
-  aboutImage:
-    "https://images.unsplash.com/photo-1641178517999-b9c2dadcde86?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
-
   //   End About Section ---------------------
 
   // Skills Section ---------------
